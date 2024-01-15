f91cdfc1317ca2d1f902ea3dc302b81cd908f9ff
PinKevin <emeriokevin2320@gmail.com>
2024-01-15 16:47:57 +0700

    fix: create berkas

--- app/Livewire/BerkasLivewire.php
+++ app/Livewire/BerkasLivewire.php
@@ -5,17 +5,65 @@ namespace App\Livewire;
 use App\Models\Berkas;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\DB;
 
 class BerkasLivewire extends Component
 {
-    use WithPagination;
+    use WithPagination, WithFileUploads;
 
     public $nama, $no_rekening, $file_bukti, $tanggal_pengambilan;
+
+    public function rules()
+    {
+        return [
+            'nama' => 'required|min:5|string',
+            'no_rekening' => 'required|min:5|unique:berkas,no_rekening',
+            'tanggal_pengambilan' => 'required|date',
+            'file_bukti' => 'required|file'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => ':attribute harus diisi!',
+            'min' => ':attribute harus terdiri atas :min karakter!',
+            'string' => ':attribute hanya terdiri atas huruf!',
+            'unique' => ':attribute sudah ada di dalam database!',
+            'date' => ':attribute harus berupa tanggal!'
+        ];
+    }
+
+    public function validationAttributes()
+    {
+        return [
+            'nama' => 'Nama debitur',
+            'no_rekening' => 'Nomor debitur',
+            'tanggal_pengambilan' => 'Tanggal pengambilan',
+            'file_bukti' => 'Bukti pengambilan'
+        ];
+    }
 
     public function indexBerkas()
     {
         $berkas = Berkas::select('id', 'nama', 'no_rekening')->paginate(10);
         return $berkas;
+    }
+
+    public function createBerkas()
+    {
+        $this->validate();
+
+        $namaFile = "bukti_" . strtolower(str_replace(' ', '_', $this->nama)) . ".pdf";
+        $this->file_bukti->storeAs('file_bukti', $namaFile);
+
+        Berkas::create([
+            'nama' => $this->nama,
+            'no_rekening' => $this->no_rekening,
+            'tanggal_pengambilan' => $this->tanggal_pengambilan,
+            'file_bukti' => $namaFile,
+        ]);
     }
 
     public function showBerkas($id)

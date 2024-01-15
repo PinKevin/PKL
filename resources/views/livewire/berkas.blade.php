fc45648cb9e78fe3855f0f21497eb89d160ce33a
sulthanfirmansyah <sulthanfirmansyah03@gmail.com>
2024-01-15 14:59:39 +0700

    berkas

--- resources/views/livewire/berkas.blade.php
+++ resources/views/livewire/berkas.blade.php
@@ -80,7 +80,7 @@
                     </svg>
                 </div>
                 <input
-                    class="block w-80 rounded-lg border border-gray-300 bg-gray-50 ps-10 pt-2 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                    class="block w-96 rounded-lg border border-gray-300 bg-gray-50 ps-10 pt-2 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                     id="table-search" type="text" placeholder="Search for items">
             </div>
         </div>
@@ -102,6 +102,7 @@
                     <th class="px-6 py-3" scope="col">
                         Aksi
                     </th>
+                    
                 </tr>
             </thead>
             <tbody>

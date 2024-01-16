188fc7b2dae056967b7b1b70632cd92286576768
PinKevin <emeriokevin2320@gmail.com>
2024-01-16 11:22:55 +0700

    feat: sort, fix: tanggal di tabel

--- resources/views/livewire/berkas/berkas-livewire.blade.php
+++ resources/views/livewire/berkas/berkas-livewire.blade.php
@@ -94,26 +94,34 @@
                         No
                     </th>
                     <th class="px-7 py-4" scope="col">
-                        <div class="flex items-center">
+                        <button class="flex items-center uppercase" wire:click="sortResult('nama')">
                             Nama Debitur
-                            <a href="#"><svg class="ms-1.5 h-3 w-3" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                    <path
-                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                                </svg></a>
-                        </div>
-
+                            <svg class="ms-1.5 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 24 24">
+                                <path
+                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                            </svg>
+                        </button>
                     </th>
                     <th class="px-7 py-4" scope="col">
-                        <div class="flex items-center">
+                        <button class="flex items-center uppercase" wire:click="sortResult('no_rekening')">
                             Nomor Debitur
-                            <a href="#"><svg class="ms-1.5 h-3 w-3" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                    <path
-                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                                </svg></a>
-                        </div>
-
+                            <svg class="ms-1.5 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 24 24">
+                                <path
+                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                            </svg>
+                        </button>
+                    </th>
+                    <th class="px-7 py-4" scope="col">
+                        <button class="flex items-center uppercase" wire:click="sortResult('tanggal_pengambilan')">
+                            Tanggal
+                            <svg class="ms-1.5 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 24 24">
+                                <path
+                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                            </svg>
+                        </button>
                     </th>
                     <th class="px-7 py-4" scope="col">
                         Aksi
@@ -134,9 +142,13 @@
                         <td class="px-6 py-4">
                             {{ $bks->no_rekening }}
                         </td>
+                        <td class="px-6 py-4">
+                            {{ $bks->tanggal_pengambilan }}
+                        </td>
                         <td class="flex justify-between px-6 py-4">
-                            <button id="button-show-modal" data-modal-target="show-modal" data-modal-toggle="show-modal"
-                                type="button" wire:click="showBerkas({{ $bks->id }})">
+                            <button id="button-show-modal" data-modal-target="show-modal"
+                                data-modal-toggle="show-modal" type="button"
+                                wire:click="showBerkas({{ $bks->id }})">
                                 <svg class="h-4 w-4 text-gray-600 hover:text-gray-900 dark:text-white"
                                     aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                                     viewBox="0 0 20 20">
@@ -157,7 +169,7 @@
                                 </svg>
                                 <div class="sr-only">Edit</div>
                             </button>
-                            <button class="" id="button-delete-modal" data-modal-target="delete-modal"
+                            <button id="button-delete-modal" data-modal-target="delete-modal"
                                 data-modal-toggle="delete-modal" type="button"
                                 wire:click="deleteBerkas({{ $bks->id }})">
                                 <svg class="h-4 w-4 text-red-600 hover:text-red-900 dark:text-white"

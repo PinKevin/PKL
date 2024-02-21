702aced221fa41ed2bec93863b071e966ba43a02
PinKevin <emeriokevin2320@gmail.com>
2024-02-21 11:10:24 +0700

    fix: format report peminjaman

--- app/Livewire/ReportPeminjamanLivewire.php
+++ app/Livewire/ReportPeminjamanLivewire.php
@@ -3,16 +3,11 @@
 namespace App\Livewire;
 
 use App\Models\Debitur;
-use App\Models\Dokumen;
 use Livewire\Component;
-use App\Models\BastPeminjaman;
 use PhpOffice\PhpSpreadsheet\Cell\DataType;
 use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
-
-
-// require 'vendor/autoload.php';
 
 class ReportPeminjamanLivewire extends Component
 {
@@ -27,8 +22,6 @@ class ReportPeminjamanLivewire extends Component
             ->where('nama_debitur', 'like', '%' . trim($this->nama_filter) . '%')
             ->orWhere('no_debitur', 'like', '%' . trim($this->nama_filter) . '%')
             ->get();
-
-        // dd($debiturWithDokumen);
 
         $debiturWithDokumenDipinjam = $debiturWithDokumen->map(function ($debitur) {
             $debitur->dokumen = $debitur->dokumen->filter(function ($dokumen) {
@@ -47,44 +40,7 @@ class ReportPeminjamanLivewire extends Component
             return $debitur->dokumen->isEmpty();
         });
         return $debiturWithDokumenDipinjam;
-        // dd($debiturWithDokumenDipinjam);
     }
-
-    // public function allDokumenDipinjam()
-    // {
-    //     $this->tanggal_pinjam_filter = '2024-02-15';
-    //     $this->tanggal_jatuh_tempo_filter = '2024-02-24';
-
-    //     $bastPeminjaman = BastPeminjaman::with('peminjaman.dokumen.debitur')
-    //         ->whereBetween('tanggal_pinjam', [
-    //             $this->tanggal_pinjam_filter,
-    //             $this->tanggal_jatuh_tempo_filter
-    //         ])
-    //         ->orWhereBetween('tanggal_jatuh_tempo', [
-    //             $this->tanggal_pinjam_filter,
-    //             $this->tanggal_jatuh_tempo_filter
-    //         ])
-    //         ->whereHas('peminjaman.dokumen.debitur', function ($query) {
-    //             $query->where('nama_debitur', 'like', '%' . trim($this->nama_filter) . '%')
-    //                 ->orWhere('no_debitur', 'like', '%' . trim($this->nama_filter) . '%');
-    //         })
-    //         ->get();
-
-    //     // dd($bastPeminjaman);
-
-    //     $debiturWithDokumenDipinjam = $bastPeminjaman->map(function ($bast) {
-    //         return $bast->peminjaman->map(function ($peminjaman) use ($bast) {
-    //             $dokumen = $peminjaman->dokumen;
-    //             $debitur = $dokumen->debitur;
-    //             $dokumen->tanggal_pinjam = $bast->tanggal_pinjam->format('d-m-Y');
-    //             $dokumen->tanggal_jatuh_tempo = $bast->tanggal_jatuh_tempo->format('d-m-Y');
-    //             return $dokumen;
-    //         });
-    //     })->flatten();
-
-    //     // dd($debiturWithDokumenDipinjam);
-    //     return $debiturWithDokumenDipinjam;
-    // }
 
     public function printReport()
     {
@@ -93,21 +49,31 @@ class ReportPeminjamanLivewire extends Component
         if ($data) {
             $data = $data->values();
 
-            $spreadsheet = IOFactory::load('format/format-laporan.xlsx');
+            $spreadsheet = IOFactory::load('format/format-report-peminjaman.xlsx');
             $worksheet = $spreadsheet->getActiveSheet();
+
+            $todayDate = now()->format('d-m-Y');
+
+            $worksheet->setCellValue("E2", $todayDate);
+
+            $endOfBorder = 0;
 
             $startRow = 5;
             foreach ($data as $index => $debitur) {
                 $dokumen = $debitur->dokumen->values();
                 $dokumenCount = count($dokumen);
 
+                $isLastIteration = $index === count($data) - 1;
+
                 $worksheet->setCellValue("A$startRow", $index + 1);
                 $worksheet->setCellValueExplicit("B$startRow", $debitur->no_debitur, DataType::TYPE_STRING);
                 $worksheet->setCellValue("C$startRow", $debitur->nama_debitur);
 
-                $worksheet->mergeCells("A$startRow:A" . ($startRow + $dokumenCount - 1));
-                $worksheet->mergeCells("B$startRow:B" . ($startRow + $dokumenCount - 1));
-                $worksheet->mergeCells("C$startRow:C" . ($startRow + $dokumenCount - 1));
+                $endRow = $startRow + $dokumenCount - 1;
+
+                $worksheet->mergeCells("A$startRow:A" . ($endRow));
+                $worksheet->mergeCells("B$startRow:B" . ($endRow));
+                $worksheet->mergeCells("C$startRow:C" . ($endRow));
 
                 foreach ($dokumen as $key => $dok) {
                     $worksheet->setCellValue("D" . ($startRow + $key), $dok->jenis);
@@ -116,6 +82,10 @@ class ReportPeminjamanLivewire extends Component
                 }
 
                 $startRow += $dokumenCount;
+
+                if ($isLastIteration) {
+                    $endOfBorder = $endRow;
+                }
             }
 
             $styleArray = [
@@ -124,17 +94,21 @@ class ReportPeminjamanLivewire extends Component
                         'borderStyle' => Border::BORDER_THIN,
                     ],
                 ],
+                'alignment' => [
+                    'horizontal' => Alignment::HORIZONTAL_CENTER,
+                    'vertical' => Alignment::VERTICAL_CENTER
+                ]
             ];
-            
-            $worksheet->getStyle('A4:')->applyFromArray($styleArray);
+
+            $worksheet->getStyle('A4:F' . $endOfBorder)->applyFromArray($styleArray);
 
             $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-            $writer->save('tes.xlsx');
 
-            return response()->download('tes.xlsx')->deleteFileAfterSend(true);
+            $fileName = "REPORT - PEMINJAMAN - $todayDate.xlsx";
+            $writer->save($fileName);
+
+            return response()->download($fileName)->deleteFileAfterSend(true);
         }
-
-        
     }
 
     public function render()

dc51432a23d2680c9f7cd2db2b95bde59be02f7f
PinKevin <emeriokevin2320@gmail.com>
2024-02-21 11:22:52 +0700

    feat: report pengambilan

--- app/Livewire/ReportPengambilanLivewire.php
+++ app/Livewire/ReportPengambilanLivewire.php
@@ -4,6 +4,10 @@ namespace App\Livewire;
 
 use App\Models\Debitur;
 use Livewire\Component;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 
 class ReportPengambilanLivewire extends Component
 {
@@ -32,6 +36,75 @@ class ReportPengambilanLivewire extends Component
             return $debitur->dokumen->isEmpty();
         });
         return $debiturWithDokumenDiambil;
+    }
+
+    public function printReport()
+    {
+        $data = $this->allDokumenDiambil();
+
+        if ($data) {
+            $data = $data->values();
+
+            $spreadsheet = IOFactory::load('format/format-report-pengambilan.xlsx');
+            $worksheet = $spreadsheet->getActiveSheet();
+
+            $todayDate = now()->format('d-m-Y');
+
+            $worksheet->setCellValue("E2", $todayDate);
+            $worksheet->getStyle('E2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+            $endOfBorder = 0;
+
+            $startRow = 5;
+            foreach ($data as $index => $debitur) {
+                $dokumen = $debitur->dokumen->values();
+                $dokumenCount = count($dokumen);
+
+                $isLastIteration = $index === count($data) - 1;
+
+                $worksheet->setCellValue("A$startRow", $index + 1);
+                $worksheet->setCellValueExplicit("B$startRow", $debitur->no_debitur, DataType::TYPE_STRING);
+                $worksheet->setCellValue("C$startRow", $debitur->nama_debitur);
+
+                $endRow = $startRow + $dokumenCount - 1;
+
+                $worksheet->mergeCells("A$startRow:A" . ($endRow));
+                $worksheet->mergeCells("B$startRow:B" . ($endRow));
+                $worksheet->mergeCells("C$startRow:C" . ($endRow));
+
+                foreach ($dokumen as $key => $dok) {
+                    $worksheet->setCellValue("D" . ($startRow + $key), $dok->jenis);
+                    $worksheet->setCellValue("E" . ($startRow + $key), $dok->tanggal_diambil);
+                }
+
+                $startRow += $dokumenCount;
+
+                if ($isLastIteration) {
+                    $endOfBorder = $endRow;
+                }
+            }
+
+            $styleArray = [
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => Border::BORDER_THIN,
+                    ],
+                ],
+                'alignment' => [
+                    'horizontal' => Alignment::HORIZONTAL_CENTER,
+                    'vertical' => Alignment::VERTICAL_CENTER
+                ]
+            ];
+
+            $worksheet->getStyle('A4:E' . $endOfBorder)->applyFromArray($styleArray);
+
+            $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+
+            $fileName = "REPORT - PENGAMBILAN - $todayDate.xlsx";
+            $writer->save($fileName);
+
+            return response()->download($fileName)->deleteFileAfterSend(true);
+        }
     }
 
     public function render()

ac46bb78ef9d6b39f179cf3449bf26d0581fe048
PinKevin <emeriokevin2320@gmail.com>
2024-03-04 14:07:49 +0700

    fix: ubah logo pakai favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link type="image/png" href="{{ asset('img/logo.png') }}" rel="icon" sizes="16X16">
+        <link type="image/x-icon" href="{{ asset('img/favicon.ico') }}" rel="icon">
         <title>@yield('page_title')</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @stack('styles')

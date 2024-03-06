f1abf82e89d7024ab5860f28abbe7837ad4235cf
sulthanfirmansyah <sulthanfirmansyah03@gmail.com>
2024-03-06 08:38:50 +0700

    tabel-jatemp

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,6 +14,54 @@
         <h3 class="ml-2 mt-1 text-2xl dark:text-white">{{ auth()->user()->nama }}</h3>
         <h5 class="ml-2 text-lg dark:text-white">NIP. {{ auth()->user()->nip }}</h5>
     </div>
+
+    <div class="mt-8 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm  text-left rtl:text-right text-gray-700 dark:text-gray-400">
+            <caption
+                class="p-5 border-gray-300 border-2 rounded-t-lg text-lg font-semibold text-left rtl:text-right text-gray-900 bg-gray-50 dark:text-white dark:bg-gray-800">
+                Dokumen Jatuh Tempo
+            </caption>
+            <thead class="text-xs text-gray-700 uppercase bg-slate-200 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Nomor Debitur
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nama Debitur
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Staff Notaris
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Staff Cabang
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Dokumen
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-slate-100 border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        ---
+                    </th>
+                    <td class="px-6 py-4">
+                        ---
+                    </td>
+                    <td class="px-6 py-4">
+                        ---
+                    </td>
+                    <td class="px-6 py-4">
+                        --
+                    </td>
+                    <td class="px-6 py-4">
+                        --
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
     <div class="mt-8 rounded-lg bg-gray-50">
         <div class="flex w-full items-center justify-center rounded-lg border-2 border-gray-300 p-4 dark:border-gray-700">
             <div
@@ -46,6 +94,10 @@
             </div>
         </div>
     </div>
+
+
+
+
 
     <div class="container">
         <div class="m-auto mt-10 rounded border-2 border-gray-300 bg-gray-50 p-6 shadow">

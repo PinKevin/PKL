1d8b5e0aa32fc23c3796e31971a48d96081ac07c
sulthanfirmansyah <sulthanfirmansyah03@gmail.com>
2024-03-29 16:04:43 +0700

    rev-user

--- resources/views/livewire/debitur/create-debitur-livewire.blade.php
+++ resources/views/livewire/debitur/create-debitur-livewire.blade.php
@@ -374,12 +374,13 @@
             <button
                 class="inline-flex w-full items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
                 type="submit">
-                <svg class="mr-2 h-3 w-3 text-white dark:text-white" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                <svg class="-ms-1 me-1 h-5 w-5 text-white dark:text-white" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                    viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M9 1v16M1 9h16" />
+                        d="M12 5v9m-5 0H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-2M8 9l4-5 4 5m1 8h.01" />
                 </svg>
-                Tambah Debitur
+                Simpan
             </button>
         </div>
 
